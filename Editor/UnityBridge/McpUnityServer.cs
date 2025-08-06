2ca23c3f0f0f689ee5e16e618dcb547b45bde5a2
CoderGamester <game.gamester@gmail.com>
2025-08-06 21:45:51 +0300

    Register CreatePrefabTool in Unity server
    Modified McpUnityServer.cs to register the new CreatePrefabTool in the tool list, enabling it to be recognized and used by the MCP Unity Editor.

--- Editor/UnityBridge/McpUnityServer.cs
+++ Editor/UnityBridge/McpUnityServer.cs
@@ -249,6 +249,10 @@ namespace McpUnity.Unity
             // Register AddAssetToSceneTool
             AddAssetToSceneTool addAssetToSceneTool = new AddAssetToSceneTool();
             _tools.Add(addAssetToSceneTool.Name, addAssetToSceneTool);
+            
+            // Register CreatePrefabTool
+            CreatePrefabTool createPrefabTool = new CreatePrefabTool();
+            _tools.Add(createPrefabTool.Name, createPrefabTool);
         }
         
         /// <summary>
